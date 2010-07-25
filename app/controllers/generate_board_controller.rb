fa4d53597d4487463e81453a77433f30b5515204
Shivasubramanian A <ashivasubramanian@gmail.com>
2010-07-25 14:37:24 +0530

    Game modes are displayed dynamically on the listbox.

--- app/controllers/generate_board_controller.rb
+++ app/controllers/generate_board_controller.rb
@@ -5,6 +5,12 @@ class GenerateBoardController < ApplicationController
 		mode = request.query_parameters[:mode]
 		print mode
 		board = Board.new(mode)
+		
+		mode_list = Array.new (3)
+		mode_list[0] = "Beginner"
+		mode_list[1] = "Intermediate"
+		mode_list[2] = "Advanced"
+		request.query_parameters[:modes] = mode_list
 	end
 
 end
