6a3e015ec8bcd891b12977d3ec0cec112568d6c6
Shivasubramanian A <ashivasubramanian@gmail.com>
2010-12-06 23:47:03 +0530

    Used single quotes for strings

--- app/controllers/generate_board_controller.rb
+++ app/controllers/generate_board_controller.rb
@@ -4,9 +4,9 @@ class GenerateBoardController < ApplicationController
 		board = Board.new(params[:mode])
 		
 		mode_list = Array.new (3)
-		mode_list[0] = "Beginner"
-		mode_list[1] = "Intermediate"
-		mode_list[2] = "Advanced"
+		mode_list[0] = 'Beginner'
+		mode_list[1] = 'Intermediate'
+		mode_list[2] = 'Advanced'
 		request.query_parameters[:modes] = mode_list
 	end
 
