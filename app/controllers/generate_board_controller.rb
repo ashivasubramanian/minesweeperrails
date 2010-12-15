c3b0a818ce7e2479d9e46c2919b33a4859f0d9f2
Shivasubramanian A <shivasubramanian@shivasubramanian.(none)>
2010-12-15 11:38:38 +0530

    1) Collection initialized with symbols rather than strings 2) Collection initialized in a single line

--- app/controllers/generate_board_controller.rb
+++ app/controllers/generate_board_controller.rb
@@ -1,12 +1,11 @@
+require 'Board'
+
 class GenerateBoardController < ApplicationController
 
 	def createNewBoard
 		board = Board.new(params[:mode])
 		
-		mode_list = Array.new (3)
-		mode_list[0] = 'Beginner'
-		mode_list[1] = 'Intermediate'
-		mode_list[2] = 'Advanced'
+		mode_list = [:BEGINNER, :INTERMEDIATE, :ADVANCED] 
 		request.query_parameters[:modes] = mode_list
 	end
 
