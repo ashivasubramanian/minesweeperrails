7eba2f1fb4c937dd3e4096198668da034879b532
Shivasubramanian A <ashivasubramanian@gmail.com>
2011-08-06 19:49:42 +0530

    Root of the website set to boards/new.

--- config/routes.rb
+++ config/routes.rb
@@ -48,14 +48,12 @@ Mine::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "boards#new"
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-#   match '/mine/createBoard', :controller => 'GenerateBoard', :action => 'createNewBoard'
-#   match '/mine', :controller => 'GenerateBoard', :action => 'createNewBoard', :mode => 'Beginner'
     resources :boards,  :only => [:new]
 end
