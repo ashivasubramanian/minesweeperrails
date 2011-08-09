587959be9cc6061c9e0541bbbb3ca1b705a67de2
Shivasubramanian A <ashivasubramanian@gmail.com>
2011-08-09 23:39:47 +0530

    Added new test for controller.

--- test/functional/boards_controller_test.rb
+++ test/functional/boards_controller_test.rb
@@ -8,7 +8,13 @@ class BoardsControllerTest < ActionController::TestCase
 
 	test "should set mode to Beginner if no mode is present" do
 		response = get :new
-		board = response.request.query_parameters[:board].mode
-		assert_equal board, BoardModes::BEGINNER 
+		mode = response.request.query_parameters[:board].mode
+		assert_equal BoardModes::BEGINNER, mode 
+	end
+
+	test "should set mode to requested mode" do
+		response = get :new, {:mode => "Intermediate"}
+		mode = response.request.query_parameters[:board].mode
+		assert_equal BoardModes::INTERMEDIATE, mode
 	end
 end
