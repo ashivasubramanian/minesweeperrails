cdda826744b3f7c642a4793c3a6119c0c52a7804
Shivasubramanian A <ashivasubramanian@gmail.com>
2011-08-13 19:41:13 +0530

    Added view tests

--- test/functional/boards_controller_test.rb
+++ test/functional/boards_controller_test.rb
@@ -17,4 +17,45 @@ class BoardsControllerTest < ActionController::TestCase
 		mode = response.request.query_parameters[:board].mode
 		assert_equal BoardModes::INTERMEDIATE, mode
 	end
+
+	test "it should contain a New Game link" do
+		get :new
+		assert_select "a", "New Game"
+	end
+
+	test "it should contain a New Game link with the URL pointing to the current mode" do
+		get :new
+		assert_select 'a[href=/boards/new]'
+
+		get :new, :mode => 'Intermediate'
+		assert_select 'a[href=/boards/new?mode=Intermediate]'
+
+		get :new, :mode => 'Advanced'
+		assert_select 'a[href=/boards/new?mode=Advanced]'
+	end
+
+	test "it should have a text '(in same mode)' below the New Game link" do
+		get :new
+		assert_select 'td[class=newGameCell]' do # testing span is below New Game Link
+			assert_select 'a[href=/boards/new]'
+			assert_select 'br'
+			assert_select 'span[class=sameModeText]'
+		end
+		assert_select 'td[class=newGameCell] > span', '(in same mode)' # testing value of span
+	end
+
+	test "it should contain a listbox with three modes" do
+		get :new
+		assert_select "select > option", :count => 3
+		assert_select "select > option:first-child", "Beginner"
+		assert_select "select > option:nth-child(2)", "Intermediate"
+		assert_select "select > option:last-child", "Advanced"
+	end
+
+	test "it should have mnemonics" do
+		get :new
+		assert_select 'a[href=/boards/new][accesskey=n]'
+		assert_select 'select[accesskey=c]'
+	end
+
 end
