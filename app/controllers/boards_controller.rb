1f486dfbb7fbbc7047cdda85de4213470c558ae9
Shivasubramanian A <ashivasubramanian@gmail.com>
2013-08-15 23:44:44 +0530

    Logging the state of the board.

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -15,6 +15,16 @@ class BoardsController < ApplicationController
 		@board = Board.new(mode)
 		session[request.session_options[:id]] = @board
 		
+		board_as_string = ''
+		@board.cells.each do |row|
+			row.each do |column|
+				board_as_string += column.mine_count.to_s + ' '
+			end
+			board_as_string += "\n"
+		end	
+		logger.info "Board generated for request id #{request.session_options[:id]} is:"
+		logger.info board_as_string
+
 		mode_list = [BoardModes::BEGINNER.name, BoardModes::INTERMEDIATE.name, BoardModes::ADVANCED.name] 
 		request.query_parameters[:modes] = mode_list
 	end
