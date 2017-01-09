b0eea573118e05f2aadfaa1af7ff388adc6b688b
Shivasubramanian A <ashivasubramanian@gmail.com>
2017-01-10 00:16:44 +0530

    Removing Game.status as I am not using it anywhere.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,15 +1,13 @@
 class Game
 
-	attr_accessor :board, :status
+	attr_accessor :board
 
 	def initialize(mode)
 		@board = Board.new mode
-		@status = 'IN_PROGRESS'
 	end
 
 	def reveal_cell(row, column)
-		@status = 'GAME_OVER' if (@board.cells[row][column].mine_count == -1)
-		{:status => @status, :cell => @board.cells[row][column].as_json}
+		{:cell => @board.cells[row][column].as_json}
 	end
 
 end
--- test/unit/game_test.rb
+++ test/unit/game_test.rb
@@ -6,30 +6,12 @@ require 'mocha/test_unit'
 
 class GameTest < Test::Unit::TestCase
 
-	def test_game_should_have_In_Progress_status_when_game_is_initialized
-		game = Game.new BoardModes::BEGINNER
-
-		assert_equal 'IN_PROGRESS', game.status
-	end
-
-	def test_should_have_Game_Over_status_when_user_clicks_on_mine
-		mock_board = BoardBuilder.new(BoardModes::BEGINNER).with_mine_in(1,1).build
-		Board.expects(:new).with(BoardModes::BEGINNER).returns(mock_board)
-
-		game = Game.new BoardModes::BEGINNER
-
-		game.reveal_cell(1,1)
-
-		assert_equal 'GAME_OVER', game.status
-		Board.unstub(:new)
-	end
-
 	def test_reveal_cell_should_return_a_map_with_In_Progress_game_status
 		mock_board = BoardBuilder.new(BoardModes::BEGINNER).with_mine_in(1,1).build
 		Board.expects(:new).with(BoardModes::BEGINNER).returns(mock_board)
 
 		game = Game.new BoardModes::BEGINNER
-		expected_map = {:status => 'IN_PROGRESS', :cell => {:row => 0, :column => 0, :mine_count => '1', :cell_colour => "green"}}
+		expected_map = {:cell => {:row => 0, :column => 0, :mine_count => '1', :cell_colour => "green"}}
 
 		output = game.reveal_cell(0,0)
 
@@ -42,7 +24,7 @@ class GameTest < Test::Unit::TestCase
 		Board.expects(:new).with(BoardModes::BEGINNER).returns(mock_board)
 		
 		game = Game.new BoardModes::BEGINNER
-		expected_map = {:status => 'GAME_OVER', :cell => {:row => 1, :column => 1, :mine_count => "-1", :cell_colour => nil}}
+		expected_map = {:cell => {:row => 1, :column => 1, :mine_count => "-1", :cell_colour => nil}}
 
 		output = game.reveal_cell(1,1)
 
